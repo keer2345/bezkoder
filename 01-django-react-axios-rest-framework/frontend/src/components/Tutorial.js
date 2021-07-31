941401b70b643ea263fdef4835c94627f3200615
keer2345 <keer2345@gmail.com>
2021-07-31 09:56:20 +0800

    01-django-react-axios-rest-framework

--- 01-django-react-axios-rest-framework/frontend/src/components/Tutorial.js
+++ 01-django-react-axios-rest-framework/frontend/src/components/Tutorial.js
@@ -1,11 +1,38 @@
 import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import TutorialDataService from '../services/tutorial.service'
 
 const Tutorial = () => {
+  const { id } = useParams()
   const [currentTutorial, setCurrentTutorial] = useState(null)
 
   useEffect(() => {
-    getTutorial()
-  }, [])
+    getTutorial(id)
+  }, [id])
+
+  const getTutorial = (id) => {
+    TutorialDataService.get(id)
+      .then((resp) => setCurrentTutorial(resp.data))
+      .catch((e) => console(e))
+  }
+  const onChangeTitle = (e) => {
+    setCurrentTutorial((prevState) => {
+      return { ...prevState, title: e.target.value }
+    })
+  }
+  const onChangeDescription = (e) => {
+    setCurrentTutorial((prevState) => {
+      return { ...prevState, description: e.target.value }
+    })
+  }
+  const deleteTutorial = () => {
+    TutorialDataService.delete(id).catch((e) => console.log(e))
+  }
+  const updateTutorial = () => {
+    TutorialDataService.update(id, currentTutorial)
+      // .then((resp) => console.log(resp.data))
+      .catch((e) => console.log(e))
+  }
 
   return (
     <div>
@@ -20,9 +47,32 @@ const Tutorial = () => {
                 className="form-control"
                 id="title"
                 value={currentTutorial.title}
+                onChange={onChangeTitle}
               />
             </div>
+            <div className="form-group">
+              <label htmlFor="description">Description</label>
+              <input
+                type="text"
+                className="form-control"
+                id="description"
+                value={currentTutorial.description}
+                onChange={onChangeDescription}
+              />
+            </div>
+            <div className="form-group">
+              <label>
+                <strong>Status:</strong>
+              </label>
+              {currentTutorial.published ? 'Published' : 'Pending'}
+            </div>
           </form>
+
+          <button onClick={deleteTutorial}>Delete</button>
+
+          <button type="submit" onClick={updateTutorial}>
+            Update
+          </button>
         </div>
       ) : (
         <div>
