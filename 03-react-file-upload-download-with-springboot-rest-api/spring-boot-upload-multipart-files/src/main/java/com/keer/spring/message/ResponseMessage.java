04af3bd3baa70e3f93bfee1e7199b2a32c32543e
keer2345 <keer2345@gmail.com>
2021-08-03 20:23:05 +0800

    03-react-file-upload-download-with-springboot-rest-api/spring-boot-upload-multipart-files

---
+++ 03-react-file-upload-download-with-springboot-rest-api/spring-boot-upload-multipart-files/src/main/java/com/keer/spring/message/ResponseMessage.java
@@ -0,0 +1,10 @@
+package com.keer.spring.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ResponseMessage {
+  private String message;
+}
